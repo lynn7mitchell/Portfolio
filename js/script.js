ee0951deff664595e4a137b556bef85a73ab9bad
Lynn Mitchell <lynn7mitchell@gmail.com>
2021-08-03 15:20:46 -0500

    added new nav

--- js/script.js
+++ js/script.js
@@ -1,27 +1,26 @@
-const menuToggleHamburger = document.getElementById("menuToggleHamburger");
-const menuToggleX = document.getElementById("menuToggleX");
-const navLink = document.querySelectorAll(".nav-link");
+// const menuToggleHamburger = document.getElementById("menuToggleHamburger");
+// const menuToggleX = document.getElementById("menuToggleX");
+// const navLink = document.querySelectorAll(".nav-link");
 
-console.log(navLink)
+// console.log(navLink)
 
-menuToggleHamburger.addEventListener('click', event => {
-    console.log("works")
-    document.querySelector("nav").style.display = 'block';
-    menuToggleHamburger.style.display = 'none';
-    menuToggleX.style.display = 'block';
-})
+// menuToggleHamburger.addEventListener('click', event => {
+//     console.log("works")
+//     document.querySelector("nav").style.display = 'block';
+//     menuToggleHamburger.style.display = 'none';
+//     menuToggleX.style.display = 'block';
+// })
 
-menuToggleX.addEventListener('click', event => {
-    console.log("X works")
-    document.querySelector("nav").style.display = 'none';
-    menuToggleHamburger.style.display = 'block';
-    menuToggleX.style.display = 'none';
-})
+// menuToggleX.addEventListener('click', event => {
+//     console.log("X works")
+//     document.querySelector("nav").style.display = 'none';
+//     menuToggleHamburger.style.display = 'block';
+//     menuToggleX.style.display = 'none';
+// })
 
 
-const closeNav = function(){
-    console.log("X works")
-    document.querySelector("nav").style.display = 'none';
-    menuToggleHamburger.style.display = 'block';
-    menuToggleX.style.display = 'none';
-}
+// const closeNav = function(){
+//     console.log("X works")
+//     document.querySelector("nav").style.display = 'none';
+//     menuToggleHamburger.style.display = 'block';
+//     menuToggleX.style.display = 'none';
